466bd0ed8f34a659904684d6a5724e081797189d
Max Tsao <mtsao@YVR-NBM1056.local>
2018-01-23 15:18:47 -0800

    added hover colors.

--- background.js
+++ background.js
@@ -16,9 +16,25 @@ chrome.browserAction.onClicked.addListener(function(activeTab)
   });
 });
 */
+/*
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.storage.sync.get('state', function(data) {
+    if (data.state === 'on') {
+      chrome.storage.sync.set({state: 'off'});
+      //do something, removing the script or whatever
+    } else {
+      chrome.storage.sync.set({state: 'on'});
+      //inject your script
+    }
+  });
+});
+*/
 
-chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-  if (changeInfo.status == 'complete') {
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab)
+{
+  if (changeInfo.status == 'complete')
+  {
+    //if (data.state === 'on')
     chrome.tabs.executeScript(null,
     {
       file: "content.js"
