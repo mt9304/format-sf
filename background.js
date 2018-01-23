6ed829f8810f49052ccd77d7416993cd81c9ca03
Max Tsao <mtsao@YVR-NBM1056.local>
2018-01-23 15:03:48 -0800

    Made it run automatically.

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+/*
 chrome.browserAction.onClicked.addListener(function(activeTab)
 {
   var bkg = chrome.extension.getBackgroundPage();
@@ -13,5 +14,14 @@ chrome.browserAction.onClicked.addListener(function(activeTab)
   {
     file: "content.css"
   });
-  */
 });
+*/
+
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete') {
+    chrome.tabs.executeScript(null,
+    {
+      file: "content.js"
+    });
+  }
+})
